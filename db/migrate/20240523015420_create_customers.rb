c582257e1cd93c1daa7c06e53a3b86798cc18ea9
manoel <manoeltirj@gmail.com>
2024-05-22 23:01:33 -0300

    Actions add in customers controller

--- db/migrate/20240523015420_create_customers.rb
+++ db/migrate/20240523015420_create_customers.rb
@@ -1,7 +1,8 @@
 class CreateCustomers < ActiveRecord::Migration[7.1]
   def change
     create_table :customers do |t|
-      t.string :name
+      t.string :name, :null => false
+      #Ex:- :null => false
       t.date :dob
       t.string :email
       t.string :mobile_phone
