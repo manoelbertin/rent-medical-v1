33a8237c0118248c168322f6ed9be0ecf2ed875e
manoel <manoeltirj@gmail.com>
2024-05-22 23:09:15 -0300

    updated customers controller with method private in the end

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,20 +1,33 @@
 class CustomersController < ApplicationController
+  before_action :load_customer, only: %i[show edit update destroy]
 
   def index
+    @customers = Customer.order(:name)
   end
 
   def show
   end
 
   def new
+    @customers = Customer.new
   end
 
   def create
+    @customers = Customer.new custumer_params
+  end
+
+  def edit
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def load_customer
+    @customer = Customer.find params[:id]
   end
 end
